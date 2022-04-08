fd694b84d1ef7f8388721c88d1185f7cfea38ee6
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-04-08 09:29:38 +0800

    随便乱写

--- src/script/index.ts
+++ src/script/index.ts
@@ -1,16 +1,4 @@
 import fs from "fs";
-import oicq from "oicq";
+import oicq, {Client} from "oicq";
 import path from "path";
 
-
-export class TinaSproutRobot {
-   private tinaSproutRobot: any = this.createTinaSprout();
-
-   createTinaSprout(): any {
-       if (this.tinaSproutRobot === null){
-          oicq.createClient('',[]);
-       }
-
-    }
-
-}
